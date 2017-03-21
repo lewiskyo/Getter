a07e494ab6ec80d3bd57139188e9ea3c15919bb5
lewiskyo <343966858@qq.com>
2017-03-21 22:58:01 +0800

    增加Cas锁实现 LockGuard实现 定时器头结构声明

---
+++ Tools/Lock.hpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+class CasLock
+{
+
+public:
+	CasLock()
+	{
+		mutex = 0;
+	}
+
+	void lock()
+	{
+		while (!(__sync_bool_compare_and_swap (&mutex,0, 1) ))
+		{
+
+		}
+	}
+
+	void unlock()
+	{
+		while (!(__sync_bool_compare_and_swap (&mutex,1, 0) ))
+		{
+
+		}
+	}
+
+	friend class LockGuard;
+
+private:
+	int mutex;
+};
+
+
+class LockGuard
+{
+public:
+	LockGuard(CasLock * _lock)
+	{
+		lock = _lock;
+		lock->lock();
+	}
+
+	~LockGuard()
+	{
+		lock->unlock();
+	}
+
+	private:
+		CasLock *lock;
+
+};
