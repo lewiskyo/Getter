6d3f982a9bc8567d5bb238b4198e6061ab59edd3
lewiskyo <343966858@qq.com>
2017-08-26 22:43:02 +0800

    增加log模块

---
+++ getter-src/log.cpp
@@ -0,0 +1,63 @@
+#include "log.h"
+
+using namespace std;
+
+Logger::Logger(string _log_dir):log_dir(_log_dir) {
+}
+
+void Logger::init(){
+    last_update_time = time((time_t*)NULL);
+}
+
+void Logger::run() {
+    th = thread(&Logger::work_thread, this);
+}
+
+void Logger::stop() {
+    th.join();
+}
+
+void Logger::execute_log() {
+    {
+        MyLockGuard guard(&lock);
+        processing_logs.swap(prepared_logs);
+    }
+
+    if (processing_logs.size() == 0) {
+        return;
+    }
+
+    update_log_name();
+    
+}
+
+void Logger::update_log_name() {
+    int now = time((time_t*)NULL);
+    if ( cur_log_filename.empty() and util::isSameHour(now, last_update_time)) {
+        return;
+    }
+    
+    // 更新输出日志文件
+    char tmp[64];
+    string new_filename(strftime(tmp,sizeof(tmp), "%04Y%02m%02d%H", now));
+    cur_log_filename = log_dir + tmp;
+    
+    handle = open(cur_log_filename, O_APPEND);
+    if (handle == -1) {
+        fprintf(stdout, "open logfilename %s err", cur_log_filename);
+        exit(1);
+    }
+}
+
+void Logger::log(string content) {
+    MyLockGuard guard(&lock);
+    prepared_logs.push_back(content);
+}
+
+void Logger::work_thread() {
+    while(1) {
+        execute_log();
+        usleep(1000000);
+        log("one log loop process finish\n");
+    }
+}
---
+++ getter-src/log.h
@@ -0,0 +1,39 @@
+#ifndef LOG_H
+#define LOG_G
+
+#include <string>
+#include <list>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include "lock.h"
+
+
+class Logger {
+    public:
+        Logger(std::string _log_dir);
+
+        void log(std::string);  // 增加日志接口
+        void execute_log();         // 输出日志
+
+        void init();
+        void run();
+        void stop();
+        void work_thread();
+
+    private:
+        CasLock lock;       
+        std::list<std::string> prepared_logs; // 待处理的日志列表
+        std::list<std::string> processing_logs; // 正在处理的日志列表
+        std::string log_dir;   // 日志放置路径
+        int handle;            // 当前输出文件句柄
+        int last_update_time;   // 上次日志输出时间
+        std::thread th;
+    private:
+        void update_log_name();
+
+};
+
+#endif
